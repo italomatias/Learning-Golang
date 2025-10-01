d3de7fba3ea049d7eb08dec69292d0c1a3f68f22
Italo Reis <italomatiasdosreis@gmail.com>
2025-10-01 20:25:52 -0300

    Lerning about operators and Structs

---
+++ 5 - Operadores/operadores.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// ARITIMETICOS
+	soma := 1 + 2
+	subtracao := 1 - 2
+	divisão := 1 / 2
+	multiplicacao := 1 * 2
+	resto := 1 % 2
+
+	fmt.Println(soma, subtracao, divisão, multiplicacao, resto)
+
+	// ATRIBUICAO
+	var var1 string = "Texto"
+	var2 := "Texto"
+	fmt.Println(var1, var2)
+
+	// RELACIONAIS
+	// > - Maior que
+	// < - Menor que
+	// == - Igual
+	// != - Diferente
+	// >= - Maior ou igual
+	// <= - Menor ou igual
+
+	//LOGICOS
+	// AND &&
+	// OR ||
+	// NOT !
+	//
+
+	//UNARIOS
+	numero := 10
+	numero++     // INCREMENTA MAIS 1
+	numero += 15 // INCREMENTA MAIS 15
+	numero--     // DECREMENTA MAIS 1
+	numero -= 15 // DECREMENTA MAIS 15
+	numero *= 3  // numero * 3
+	fmt.Println(numero)
+
+	//OPERADOR TERNARIO
+	// IFS
+}
---
+++ 6 - Structs/structs.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// Definindo um struct
+type usuario struct {
+	nome  string
+	idade int8
+	end   endereco
+}
+
+type endereco struct {
+	rua    string
+	numero int8
+}
+
+func main() {
+	fmt.Println("Structs")
+	// Criando um struct ele começa com valores zero caso não instanciados
+	var usuario1 usuario
+	fmt.Println("user com valor zero ", usuario1)
+	// Atribuindo valores ao struct
+	usuario1.nome = "Italo"
+	usuario1.idade = 31
+	fmt.Println(usuario1)
+
+	endereco := endereco{"Rua A", 100}
+
+	// SEGUNDA FORMA DE INSTANCIAR STRUCT
+	usuario2 := usuario{"Elizabeth", 47, endereco}
+	fmt.Println(usuario2)
+
+	// Criando usuário sem todos os dados
+	usuario3 := usuario{idade: 21}
+	fmt.Println(usuario3)
+
+}
